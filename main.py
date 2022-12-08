4aaee92a9f7385d4f2511eb4c2b677d0b1023ac4
Maverick Wong <mvrck.wong@gmail.com>
2022-12-08 17:22:24 +0800

    update main

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+##########################################
+#   ___ __  __ ___  ___  ___ _____ ___   #
+#  |_ _|  \/  | _ \/ _ \| _ |_   _/ __|  #
+#   | || |\/| |  _| (_) |   / | | \__ \  #
+#  |___|_|  |_|_|  \___/|_|_\ |_| |___/  #
+#                                        #
+##########################################
+
+#########################################################
+#   ___  ___ ___ ___ _  _ ___ _____ ___ ___  _  _ ___   #
+#  |   \| __| __|_ _| \| |_ _|_   _|_ _/ _ \| \| / __|  #
+#  | |) | _|| _| | || .` || |  | |  | | (_) | .` \__ \  #
+#  |___/|___|_| |___|_|\_|___| |_| |___\___/|_|\_|___/  #
+#                                                       #
+#########################################################
+
+#############################################
+#   ___ _____ _   _  _ ___   _   ___ ___    #
+#  / __|_   _/_\ | \| |   \ /_\ | _ |   \   #
+#  \__ \ | |/ _ \| .` | |) / _ \|   | |) |  #
+#  |___/ |_/_/ \_|_|\_|___/_/ \_|_|_|___/   #
+#                                           #
+#############################################
+
+######################################
+#   ___ ___  ___   ___ ___ ___ ___   #
+#  | _ | _ \/ _ \ / __| __/ __/ __|  # 
+#  |  _|   | (_) | (__| _|\__ \__ \  #
+#  |_| |_|_\\___/ \___|___|___|___/  #
+#                                    #
+######################################
+
+
 import httpx
 from selectolax.parser import HTMLParser
 from dataclasses import dataclass, asdict
@@ -16,6 +49,18 @@ def get_html(page):
     resp = httpx.get(url)
     return HTMLParser(resp.text)
 
+
+
+def get_thomann_html(page_no:int=1) -> HTMLParser:
+    # Convert the integer input into string.
+    if isinstance(page_no, int):
+        page_no = str(page_no)
+    
+    url = f"https://www.thomann.de/gb/search_GF_electric_guitars.html?ls=100&og={page_no}&hl=BLOWOUT"
+    response = httpx.get(url)
+    return HTMLParser(response)
+
+
 def parse_product(html):
     products = html.css("div.product")
     
@@ -29,19 +74,26 @@ def parse_product(html):
         results.append(asdict(new_item))
     return results
 
+EXPORT_CSV = False
 
-def main():
+
+def main() -> None:
     
-    #results = []
-    #for x in range(1,4):
+    # Given page number get the items in the page.
     html = get_html(1)
-    print(html.css_first("title").text())
-    res = parse_product(html)
-    #results.append(res)
+    result = parse_product(html)
+    
+    # Transfer to pandas dataframe then csv.
+    df = pd.DataFrame.from_dict(result)
+    if EXPORT_CSV:
+        df.to_csv("sample.csv")
         
-    sample = pd.DataFrame.from_dict(res)
-    sample.to_csv("sample.csv")
-    #print(sample.head(10))
+    print(df.head(20))
+    
 
 if __name__ == "__main__":
     main()
+    
+    print("-"*50)
+    print("File Executed... ")
+    print("-"*50)
